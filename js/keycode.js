c217d551856c42e774a0c0459bd5bd34f2ea2d87
Kewth <wthdsg@foxmail.com>
2018-09-22 19:19:18 +0800

    添加vim的gg按键

---
+++ js/keycode.js
@@ -0,0 +1,26 @@
+var click_g = false;
+document.onkeydown=function(event)
+{
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+	if(!e)
+	{
+		return;
+	}
+	else
+	{
+		if(e.keyCode==36)
+			location.href = '/';
+		if(e.keyCode==71)
+		{
+			if(click_g)
+			{
+				location.href = '#top';
+				click_g = false;
+			}
+			else
+				click_g = true;
+		}
+		else
+			click_g = false;
+	}
+}; 
